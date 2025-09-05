1e0cb15e639c905b88a64626c546c519907445fc
hfryan <albright_ryan@yahoo.com>
2025-09-05 17:28:42 -0400

    Fix autoloader path for global Composer installations
    - Support multiple autoloader paths for different install methods
    - Improve error handling when autoloader not found

--- bin/phpcop.php
+++ bin/phpcop.php
@@ -1,6 +1,25 @@
 #!/usr/bin/env php
 <?php
-require __DIR__ . '/../vendor/autoload.php';
+// Try multiple autoloader paths for different installation methods
+$autoloaders = [
+    __DIR__ . '/../vendor/autoload.php',      // Local development
+    __DIR__ . '/../../../autoload.php',       // Global Composer install
+    __DIR__ . '/../../../../vendor/autoload.php', // Project dependency
+];
+
+$loaded = false;
+foreach ($autoloaders as $autoloader) {
+    if (file_exists($autoloader)) {
+        require $autoloader;
+        $loaded = true;
+        break;
+    }
+}
+
+if (!$loaded) {
+    fwrite(STDERR, "Error: Could not find Composer autoloader. Please run 'composer install'.\n");
+    exit(1);
+}
 
 use PHPCop\Console\Application;
 
